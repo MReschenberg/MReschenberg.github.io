5e8f8a3feb422720d13cf91fee254e0afe596aea
Morgan Reschenberg <mreschenberg@berkeley.edu>
2017-01-08 11:44:09 -0800

    updated copyright

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,7 +1,16 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "150px";
+    document.getElementById("maintext").style.marginLeft = "150px";
+    document.getElementById("maintext").style.width = "80%";
+    document.getElementById("sidebar").style.dispaly = 'none';
+
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("maintext").style.marginLeft = "50px";
+    document.getElementById("maintext").style.width = "90%";
+    document.getElementById("sidebar").style.dispaly = '';
+
+
 }
