f422b5ef407a4bc418f3a34bbc3b91dbd9835232
Morgan Reschenberg <mreschenberg@berkeley.edu>
2017-01-08 12:22:08 -0800

    transition times for menu made smooth

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,16 +1,13 @@
 function openNav() {
+    document.getElementById("sidebar").style.transition = ".2s";
+   document.getElementById("mySidenav").style.transition = "1.5s";
+	document.getElementById("sidebar").style.width = "0px";
     document.getElementById("mySidenav").style.width = "150px";
-    document.getElementById("maintext").style.marginLeft = "150px";
-    document.getElementById("maintext").style.width = "80%";
-    document.getElementById("sidebar").style.dispaly = 'none';
-
 }
 
 function closeNav() {
+    document.getElementById("mySidenav").style.transition = ".2s";
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("maintext").style.marginLeft = "50px";
-    document.getElementById("maintext").style.width = "90%";
-    document.getElementById("sidebar").style.dispaly = '';
-
-
+    document.getElementById("sidebar").style.transition = "1.5s";
+    document.getElementById("sidebar").style.width = "50px";
 }
