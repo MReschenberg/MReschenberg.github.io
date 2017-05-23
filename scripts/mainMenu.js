3841b5c79afaee82a69cd67ce0383cbed8308f60
Morgan Reschenberg <mreschenberg@berkeley.edu>
2017-05-23 09:03:16 -0700

    update LinkedIn icon path, new sidebar for main and pushcontent sidebar for aux pages

--- scripts/mainMenu.js
+++ scripts/mainMenu.js
@@ -1,10 +1,8 @@
 function openNav() {
     document.getElementById("sidebar").style.transition = ".2s";
-   	document.getElementById("mySidenav").style.transition = "1.5s";
+   document.getElementById("mySidenav").style.transition = "1.5s";
 	document.getElementById("sidebar").style.width = "0px";
     document.getElementById("mySidenav").style.width = "150px";
-    document.getElementById("maintext").style.transition = "1.5s";
-    document.getElementById("maintext").style.marginLeft = "150px";
 }
 
 function closeNav() {
@@ -12,7 +10,4 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("sidebar").style.transition = "1.5s";
     document.getElementById("sidebar").style.width = "50px";
-    document.getElementById("maintext").style.transition = ".2s";
-    document.getElementById("maintext").style.marginLeft = "50px";
-
 }
