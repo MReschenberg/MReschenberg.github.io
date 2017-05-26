8bc518db1ec7e14e6f0194f0bca023eac7509e40
Morgan Reschenberg <mreschenberg@berkeley.edu>
2017-05-26 13:15:55 -0700

    URLs chagned for proj child pages to projects/child.\/

--- website/main/urls.py
+++ website/main/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     url(r'^about/$', views.AboutPageView.as_view()), 
     url(r'^projects/$', views.ProjectsPageView.as_view()), 
     url(r'^organisations/$', views.OrgPageView.as_view()), 
-    url(r'^calteach/$', views.CalTeachPageView.as_view()), 
-    url(r'^cs/$', views.CSPageView.as_view()), 
-    url(r'^photography/$', views.PhotogPageView.as_view()), 
+    url(r'^projects/calteach/$', views.CalTeachPageView.as_view()), 
+    url(r'^projects/cs/$', views.CSPageView.as_view()), 
+    url(r'^projects/photography/$', views.PhotogPageView.as_view()), 
 
 ]
