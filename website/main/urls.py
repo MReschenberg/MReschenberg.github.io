ef891c4b7674fcba5abc3e9e2a298b5883499762
Morgan Reschenberg <mreschenberg@berkeley.edu>
2017-05-26 14:19:37 -0700

    Index is behaving as markdown file for some reason

--- website/main/urls.py
+++ website/main/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from main import views
 
 urlpatterns = [
-    url(r'^$', views.HomePageView.as_view()),
+    url(r'^/', views.HomePageView.as_view()),
     url(r'^about/$', views.AboutPageView.as_view()), 
     url(r'^projects/$', views.ProjectsPageView.as_view()), 
     url(r'^organisations/$', views.OrgPageView.as_view()), 
