ba8a17a22a9090416dfec3f3d9635eedc49b16a1
Morgan Reschenberg <mreschenberg@berkeley.edu>
2017-08-14 22:08:34 -0700

    Change image, script paths.

---
+++ js/mainMenu.js
@@ -0,0 +1,13 @@
+function openNav() {
+    document.getElementById("sidebar").style.transition = ".2s";
+   	document.getElementById("mySidenav").style.transition = "1.5s";
+	document.getElementById("sidebar").style.width = "0px";
+    document.getElementById("mySidenav").style.width = "150px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.transition = ".2s";
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("sidebar").style.transition = "1.5s";
+    document.getElementById("sidebar").style.width = "50px";
+}
